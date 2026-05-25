24ad08d215098b72d5e7fa35f80cd3228d2896c2
Manu Sporny <msporny@digitalbazaar.com>
2026-05-25 12:21:00 -0400

    Update config name for ECDSA key identifier.

--- configs/witness.js
+++ configs/witness.js
@@ -13,10 +13,10 @@ cfg.kms.ipAllowList = ['127.0.0.1/32', '::1/128'];
 cfg.kms.meterId =
   'https://localhost:22443/meters/z19ygjQcNmQ9AbG7hCF39Kizs';
 
-// stable IDs for the witness signing keystore and key
+// stable IDs for the witness signing keystore and keys
 cfg.signing.keystoreId =
   `${cfg.kms.baseUrl}/keystores/hmbd-witness-keystore-v1`;
-cfg.signing.keyId =
-  `${cfg.signing.keystoreId}/keys/hmbd-witness-signing-key-v1`;
+cfg.signing.ecdsaKeyId =
+  `${cfg.signing.keystoreId}/keys/hmbd-witness-ecdsa-signing-key-v1`;
 cfg.signing.mldsaKeyId =
   `${cfg.signing.keystoreId}/keys/hmbd-witness-mldsa-signing-key-v1`;
