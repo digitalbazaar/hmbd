9bff954799a35943c5dd3a27b689ebaa6bec9ff8
Manu Sporny <msporny@digitalbazaar.com>
2026-06-11 18:15:49 -0400

    Migrate to computed configs for witness config.

--- configs/meter-usage-reporter.js
+++ configs/meter-usage-reporter.js
@@ -1,8 +1,12 @@
 /*!
  * Copyright (c) 2024-2026 Digital Bazaar, Inc.
  */
-import {config} from '@bedrock/core';
+import {config, util} from '@bedrock/core';
 
-const cfg = config['meter-usage-reporter'];
-
-cfg.meterServiceAllowList = ['https://localhost:22443'];
+const c = util.config.main;
+c.setComputed('meter-usage-reporter.meterServiceAllowList', () => {
+  return [
+    'http://' + config.server.domain + ':' + config.server.httpPort,
+    'https://' + config.server.domain + ':' + config.server.port
+  ];
+});
