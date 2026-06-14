7fa44e1c3f7efecb225a058217e7a176a3f3eca7
Manu Sporny <msporny@digitalbazaar.com>
2026-06-14 14:47:28 -0400

    Remove https-agent, which is no longer needed.

--- configs/https-agent.js
+++
@@ -1,7 +0,0 @@
-/*!
- * Copyright (c) 2023-2026 Digital Bazaar, Inc.
- */
-import {config} from '@bedrock/core';
-
-// allow self-signed certificates in dev
-config['https-agent'].rejectUnauthorized = false;
